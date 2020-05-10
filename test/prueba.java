30afbb18a5c1029e6d8f9536f076690afdaf5a99
Gabriela Zavala <Propietario@pc-1559579848>
2020-05-10 11:20:21 +0200

    ufff ahora si

---
+++ test/prueba.java
@@ -0,0 +1,21 @@
+package geometria;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class prueba {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("No implementado aun");
+		public double area() {
+			return radio * radio * PI;
+	}
+
+}
